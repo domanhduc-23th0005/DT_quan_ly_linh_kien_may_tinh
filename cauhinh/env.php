2dd9f057088b3f9f2781c0d7ad6400450b89a146
nt23cth-dangthanhhoa-23th0008 <hoa.dt.nt23cth@ntu.edu.vn>
2025-05-03 08:22:46 +0700

    Create env.php

---
+++ cauhinh/env.php
@@ -0,0 +1,13 @@
+<?php
+// 1. Kết nối CSDL kiểu thủ tục
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quanlylinhkien";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if (!$conn) {
+	die("Kết nối thất bại");
+}
+?>
